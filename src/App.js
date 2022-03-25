221ce90a4ca8b1005566d1d85b51a2249738e1b7
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-03-25 22:15:01 +0600

    bootstrap and font awesom added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  
+      <h1 className='text-primary'>Shop For Ramadan</h1>
     </div>
   );
 }
